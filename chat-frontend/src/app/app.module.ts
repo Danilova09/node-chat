afcdd52ce875deecc95b8f0fde1b7c156da48eb5
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-01-29 15:18:42 +0600

    Generation of main components

--- chat-frontend/src/app/app.module.ts
+++ chat-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageItemComponent } from './messages/message-item/message-item.component';
+import { MessageFormComponent } from './message-form/message-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent,
+    MessageItemComponent,
+    MessageFormComponent
   ],
   imports: [
     BrowserModule,
